a60cf7d08dc4f8a9f02df2420b892838e2346986
Arkkan E <arkkanito@gmail.com>
2021-02-01 02:11:55 -0500

    Added Git Links

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -8,23 +8,23 @@ function Projects() {
                 <h2><u><b>Projects</b></u></h2>
             </div>
             <div className='projects__content__container'>
-                <a href='#' className='projects__tile' target='_blank' >
+                <a href='https://github.com/ArkkanElkhatib/Python-BouncyBalls' className='projects__tile' target='_blank' >
                     <img className='projects__tile__img' src='PyBalls.PNG' />
                     <section>Py-Balls</section>
                 </a>
-                <a href='#' className='projects__tile' target='_blank' >
+                <a href='https://github.com/ArkkanElkhatib/Python-CoronaParser' className='projects__tile' target='_blank' >
                     <img className='projects__tile__img' src='Py-Rona.png' />
                     <section>Python Corona Scraper</section>
                 </a>
-                <a href='#' className='projects__tile' target='_blank' >
+                <a href='https://github.com/ArkkanElkhatib/React-Portfolio/tree/dark/public' className='projects__tile' target='_blank' >
                     <img className='projects__tile__img' src='Portfolio.png' />
                     <section>React Portfolio</section>
                 </a>
-                <a href='#' className='projects__tile' target='_blank' >
+                <a href='https://github.com/ArkkanElkhatib/Web-MovieDatabase' className='projects__tile' target='_blank' >
                     <img className='projects__tile__img' src='ArkDB.png' />
                     <section >Web Movie Database</section>
                 </a>
-                <a href='#' className='projects__tile' target='_blank' >
+                <a href='https://github.com/ArkkanElkhatib/JavaFX-Store' className='projects__tile' target='_blank' >
                     <img className='projects__tile__img' src='Store.png' />
                     <section className='dark' >JavaFX Store</section>
                 </a>
